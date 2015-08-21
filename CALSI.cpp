5f404d4d3053e9170b72ec8c4b8db0211a30db7a
sonali22 <shinagresonali22@gmail.com>
2015-08-21 14:40:14 +0530

    chng

--- CALSI.cpp
+++ CALSI.cpp
@@ -2,11 +2,14 @@
 #include<math.h>
 int main()
 {
-  int s1,s2,s3;
+int s1,s2;
+int add;
 cout<<"calsi program";
 cout<<"enter the numbers";
 cin>>s1;
 cout<<"enter second number";
 cin>>s2;
-s3=s1+s2;
+add=s1+s2;
+///my program
+
 }
